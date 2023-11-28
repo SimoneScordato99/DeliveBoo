684839c9906a5e1cb264fd5ea359903401b6183d
SimoneScordato99 <scordato.simone@yahoo.com>
2023-11-28 17:27:11 +0100

    migrations + models + controllers

---
+++ DeliveBoo-Back/app/Http/Controllers/CategoryRestaurantController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category_Restaurant;
+use Illuminate\Http\Request;
+
+class CategoryRestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category_Restaurant  $category_Restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category_Restaurant $category_Restaurant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category_Restaurant  $category_Restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category_Restaurant $category_Restaurant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category_Restaurant  $category_Restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category_Restaurant $category_Restaurant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category_Restaurant  $category_Restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category_Restaurant $category_Restaurant)
+    {
+        //
+    }
+}
